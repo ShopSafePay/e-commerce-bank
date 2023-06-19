6f1578c770d74aa392ad028001f74a65e5499a23
cloudy-day <mahinuralam1108@gmail.com>
2023-06-20 02:58:25 +0600

    silly change

--- app/api/payment/route.js
+++ app/api/payment/route.js
@@ -1,48 +1,49 @@
-import db from '@lib/db'
-import User from '@models/User'
-import { NextResponse } from 'next/server'
+import db from "@lib/db";
+import User from "@models/User";
+import { NextResponse } from "next/server";
 
 export const POST = async (req) => {
+  try {
+    await db();
 
-    try{
+    const { account, key, Total, recieverAccount } = await req.json();
+    let user = await User.findOne({ account });
 
-        await db()
+    let ecom = await User.findOne({ account: recieverAccount });
 
-        const { account, key, Total , recieverAccount } = await req.json()
-        let user = await User.findOne({ account })
+    console.log(user);
+    console.log(key);
 
-        let ecom = await User.findOne({ account: recieverAccount })
-
-        console.log(user)
-        console.log(key)
-        
-
-        if(!user) {
-            return new NextResponse(JSON.stringify({data: "Invalid credentials"}), {status: 401})
-        }
-
-        if(user.key !== key) {
-            return new NextResponse(JSON.stringify({data: "Wrong credentials"}), {status: 401})
-        }
-        let bal = parseInt(user.balance);
-        let temp = parseInt(Total)
-        if(bal < temp) {
-            return new NextResponse(JSON.stringify({data: "Insufficient funds"}), {status: 401})
-        }
-
-        user.balance = bal - temp;
-        ecom.balance = parseInt(ecom.balance) + temp;
-        
-        await user.save()
-        await ecom.save()
-
-        return new NextResponse(JSON.stringify({data: "Success"}), {status: 201})
-
-
-
-    } catch (err) {
-        console.log(err)
-        return new NextResponse(err, {status: 500})
+    if (!user) {
+      return new NextResponse(JSON.stringify({ data: "Invalid credentials" }), {
+        status: 401,
+      });
     }
 
-}
+    if (user.key !== key) {
+      return new NextResponse(JSON.stringify({ data: "Wrong credentials" }), {
+        status: 401,
+      });
+    }
+    let bal = parseInt(user.balance);
+    let temp = parseInt(Total);
+    if (bal < temp) {
+      return new NextResponse(JSON.stringify({ data: "Insufficient funds" }), {
+        status: 401,
+      });
+    }
+
+    user.balance = bal - temp;
+    ecom.balance = parseInt(ecom.balance) + temp;
+
+    await user.save();
+    await ecom.save();
+
+    return new NextResponse(JSON.stringify({ data: "Success" }), {
+      status: 201,
+    });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(err, { status: 500 });
+  }
+};
--- app/page.jsx
+++ app/page.jsx
@@ -1,28 +1,21 @@
-'use client'
+"use client";
 
-import Footer from "@components/Footer/Footer"
-import Hero from "@components/Hero/Hero"
-import Navbar from "@components/Navbar/Navbar"
-import Service from "@components/Service/Service"
-import Testimonial from "@components/Testimonial/Testimonial"
-
-
+import Footer from "@components/Footer/Footer";
+import Hero from "@components/Hero/Hero";
+import Navbar from "@components/Navbar/Navbar";
+import Service from "@components/Service/Service";
+import Testimonial from "@components/Testimonial/Testimonial";
 
 const Home = () => {
-
-
-
   return (
-  
     <div>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
       <Service />
-      <Testimonial/>
-      <Footer/>
+      <Testimonial />
+      <Footer />
     </div>
-  
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
